3f090846e01ae32192809e5d2f09149862331455
Bartlomiej Rey <bartlomiej.rey@gmail.com>
2021-07-28 12:57:28 +0300

    Wed 28 Jul 12:57:28 EEST 2021

--- gitpush.sh
+++ gitpush.sh
@@ -2,4 +2,4 @@
 git pull
 git add .
 git commit -m "`date`"
-git push https://meosr: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@github.com/meosr/islands.git main
+git push https://meosr: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@github.com/meosr/islands.git main
